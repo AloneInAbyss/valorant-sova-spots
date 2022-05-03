476dbd48bd5c448837248aaa88e8c63d9f27c5ea
aloneinabyss <thiago-assi@outlook.com>
2022-05-02 21:09:33 -0300

    populated spots data

--- src/data/maps_spots.tsx
+++ src/data/maps_spots.tsx
@@ -10,15 +10,112 @@ const MapsSpots = {
           'img/spots/breeze/attack_01/03.png',
         ],
       },
+      {
+        name: 'Double Door',
+        thumbnail: 'img/spots/breeze/attack_02.png',
+        images: [
+          'img/spots/breeze/attack_02/01.png',
+          'img/spots/breeze/attack_02/02.png',
+          'img/spots/breeze/attack_02/03.png',
+        ],
+      },
+      {
+        name: 'Bomb A',
+        thumbnail: 'img/spots/breeze/attack_03.png',
+        images: [
+          'img/spots/breeze/attack_03/01.png',
+          'img/spots/breeze/attack_03/02.png',
+          'img/spots/breeze/attack_03/03.png',
+          'img/spots/breeze/attack_03/04.png',
+        ],
+      },
+      {
+        name: 'Bomb A Close Corners',
+        thumbnail: 'img/spots/breeze/attack_04.png',
+        images: [
+          'img/spots/breeze/attack_04/01.png',
+          'img/spots/breeze/attack_04/02.png',
+          'img/spots/breeze/attack_04/03.png',
+          'img/spots/breeze/attack_04/04.png',
+        ],
+      },
+      {
+        name: 'Bomb A Doors and Back',
+        thumbnail: 'img/spots/breeze/attack_05.png',
+        images: [
+          'img/spots/breeze/attack_05/01.png',
+          'img/spots/breeze/attack_05/02.png',
+          'img/spots/breeze/attack_05/03.png',
+        ],
+      },
+      {
+        name: 'Bomb B Shock Dart',
+        thumbnail: 'img/spots/breeze/attack_06.png',
+        images: [
+          'img/spots/breeze/attack_06/01.png',
+          'img/spots/breeze/attack_06/02.png',
+          'img/spots/breeze/attack_06/03.png',
+          'img/spots/breeze/attack_06/04.png',
+          'img/spots/breeze/attack_06/05.png',
+        ],
+      },
     ],
     defense: [
       {
-        name: 'Double Door',
+        name: 'B Lobby',
         thumbnail: 'img/spots/breeze/defense_01.png',
         images: [
           'img/spots/breeze/defense_01/01.png',
           'img/spots/breeze/defense_01/02.png',
           'img/spots/breeze/defense_01/03.png',
+        ],
+      },
+      {
+        name: 'B Lobby',
+        thumbnail: 'img/spots/breeze/defense_02.png',
+        images: [
+          'img/spots/breeze/defense_02/01.png',
+          'img/spots/breeze/defense_02/02.png',
+          'img/spots/breeze/defense_02/03.png',
+        ],
+      },
+      {
+        name: 'A Cave',
+        thumbnail: 'img/spots/breeze/defense_03.png',
+        images: [
+          'img/spots/breeze/defense_03/01.png',
+          'img/spots/breeze/defense_03/02.png',
+          'img/spots/breeze/defense_03/03.png',
+        ],
+      },
+      {
+        name: 'Bomb B Shock Dart',
+        thumbnail: 'img/spots/breeze/defense_04.png',
+        images: [
+          'img/spots/breeze/defense_04/01.png',
+          'img/spots/breeze/defense_04/02.png',
+          'img/spots/breeze/defense_04/03.png',
+          'img/spots/breeze/defense_04/04.png',
+        ],
+      },
+      {
+        name: 'Bomb A Shock Dart',
+        thumbnail: 'img/spots/breeze/defense_05.png',
+        images: [
+          'img/spots/breeze/defense_05/01.png',
+          'img/spots/breeze/defense_05/02.png',
+          'img/spots/breeze/defense_05/03.png',
+          'img/spots/breeze/defense_05/04.png',
+        ],
+      },
+      {
+        name: 'Bomb A Shock Dart',
+        thumbnail: 'img/spots/breeze/defense_06.png',
+        images: [
+          'img/spots/breeze/defense_06/01.png',
+          'img/spots/breeze/defense_06/02.png',
+          'img/spots/breeze/defense_06/03.png',
+          'img/spots/breeze/defense_06/04.png',
         ],
       },
     ],
