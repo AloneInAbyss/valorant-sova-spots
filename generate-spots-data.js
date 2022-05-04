2fb24dfdba2e48da71960682d66e3d1f50bba44f
aloneinabyss <thiago-assi@outlook.com>
2022-05-03 21:14:44 -0300

    updated feature for generating spots data

--- generate-spots-data.js
+++ generate-spots-data.js
@@ -7,32 +7,79 @@ const baseDir = path.join(__dirname + '/public/img/spots');
 generateSpotsData(baseDir);
 
 async function generateSpotsData(dir) {
+  const mapsList = await getMapsNames(dir);
+
+  const mapsInfo = await getMapsInformation(mapsList);
+
+  await writeInfoOnFile(mapsInfo);
+}
+
+async function getMapsNames(dir) {
   let mapsList;
 
   await getDirectoriesNames(dir).then((res) => {
     mapsList = res;
   });
 
+  return mapsList;
+}
+
+async function getMapsInformation(mapsList) {
+  const information = {};
+
   // Percorre um array com os nomes dos mapas
-  mapsList.forEach(async (map) => {
+  mapsList.forEach((map) => {
+    information[map] = {};
     let dir = path.join(baseDir, '/', map);
 
     // Percorre um array para ataque e para defesa
     ['attack', 'defense'].forEach(async (type) => {
+      information[map][type] = [];
       let dirType = path.join(dir, '/', type);
 
-      await getDirectoriesNames(dirType).then((spotsNumbers) => {
+      await getDirectoriesNames(dirType).then((spotsNumbersAndNames) => {
         // Percorre um array para cada spot
-        spotsNumbers.forEach(async (spotNumber) => {
-          let spotName = spotNumber.slice(3);
-          let number = await getNumberOfFiles(
-            path.join(dirType, '/', spotNumber)
-          );
-          console.log(number);
+        spotsNumbersAndNames.forEach(async (spotNumberAndName, index) => {
+          let dirSpot = path.join(dirType, '/', spotNumberAndName);
+
+          let spotName = spotNumberAndName.slice(3);
+          let thumbnail = `img/spots/${map}/${type}/${spotNumberAndName}/logo.png`;
+          let images = [];
+
+          let filesNumber = await getNumberOfFiles(dirSpot);
+          for (let i = 1; i <= filesNumber; i++) {
+            let spot;
+
+            if (i < 10) {
+              spot = `img/spots/${map}/${type}/${spotNumberAndName}/0${i}.png`;
+            } else {
+              spot = `img/spots/${map}/${type}/${spotNumberAndName}/${i}.png`;
+            }
+
+            images.push(spot);
+          }
+
+          information[map][type][index] = {
+            name: spotName,
+            thumbnail,
+            images,
+          };
         });
       });
     });
   });
+
+  return information;
+}
+
+async function writeInfoOnFile(mapsInfo) {
+  setTimeout(() => {
+    let data = 'const MapsSpots = ';
+    data += JSON.stringify(mapsInfo, null, 2);
+    data += '; export default MapsSpots;';
+
+    fs.writeFileSync('./src/data/maps_spots.tsx', data, 'utf-8');
+  }, 1000);
 }
 
 async function getDirectoriesNames(dir) {
--- src/data/maps_spots.tsx
+++ src/data/maps_spots.tsx
@@ -1,28 +1,135 @@
 const MapsSpots = {
-  breeze: {
-    attack: [
+  "breeze": {
+    "attack": [
       {
-        name: 'Bomb B',
-        thumbnail: 'img/spots/breeze/attack/01_Bomb B/logo.png',
-        images: [
-          'img/spots/breeze/attack/01_Bomb B/01.png',
-          'img/spots/breeze/attack/01_Bomb B/02.png',
-          'img/spots/breeze/attack/01_Bomb B/03.png',
-        ],
+        "name": "Bomb B",
+        "thumbnail": "img/spots/breeze/attack/01_Bomb B/logo.png",
+        "images": [
+          "img/spots/breeze/attack/01_Bomb B/01.png",
+          "img/spots/breeze/attack/01_Bomb B/02.png",
+          "img/spots/breeze/attack/01_Bomb B/03.png",
+          "img/spots/breeze/attack/01_Bomb B/04.png"
+        ]
       },
-    ],
-    defense: [
       {
-        name: 'B Lobby',
-        thumbnail: 'img/spots/breeze/defense/01_B Lobby/logo.png',
-        images: [
-          'img/spots/breeze/defense/01_B Lobby/01.png',
-          'img/spots/breeze/defense/01_B Lobby/02.png',
-          'img/spots/breeze/defense/01_B Lobby/03.png',
-        ],
+        "name": "Double Door",
+        "thumbnail": "img/spots/breeze/attack/02_Double Door/logo.png",
+        "images": [
+          "img/spots/breeze/attack/02_Double Door/01.png",
+          "img/spots/breeze/attack/02_Double Door/02.png",
+          "img/spots/breeze/attack/02_Double Door/03.png",
+          "img/spots/breeze/attack/02_Double Door/04.png"
+        ]
       },
+      {
+        "name": "Bomb A",
+        "thumbnail": "img/spots/breeze/attack/03_Bomb A/logo.png",
+        "images": [
+          "img/spots/breeze/attack/03_Bomb A/01.png",
+          "img/spots/breeze/attack/03_Bomb A/02.png",
+          "img/spots/breeze/attack/03_Bomb A/03.png",
+          "img/spots/breeze/attack/03_Bomb A/04.png",
+          "img/spots/breeze/attack/03_Bomb A/05.png"
+        ]
+      },
+      {
+        "name": "Bomb A Close Corners",
+        "thumbnail": "img/spots/breeze/attack/04_Bomb A Close Corners/logo.png",
+        "images": [
+          "img/spots/breeze/attack/04_Bomb A Close Corners/01.png",
+          "img/spots/breeze/attack/04_Bomb A Close Corners/02.png",
+          "img/spots/breeze/attack/04_Bomb A Close Corners/03.png",
+          "img/spots/breeze/attack/04_Bomb A Close Corners/04.png",
+          "img/spots/breeze/attack/04_Bomb A Close Corners/05.png"
+        ]
+      },
+      {
+        "name": "Bomb A Doors and Back",
+        "thumbnail": "img/spots/breeze/attack/05_Bomb A Doors and Back/logo.png",
+        "images": [
+          "img/spots/breeze/attack/05_Bomb A Doors and Back/01.png",
+          "img/spots/breeze/attack/05_Bomb A Doors and Back/02.png",
+          "img/spots/breeze/attack/05_Bomb A Doors and Back/03.png",
+          "img/spots/breeze/attack/05_Bomb A Doors and Back/04.png"
+        ]
+      },
+      {
+        "name": "Bomb B Shock Dart",
+        "thumbnail": "img/spots/breeze/attack/06_Bomb B Shock Dart/logo.png",
+        "images": [
+          "img/spots/breeze/attack/06_Bomb B Shock Dart/01.png",
+          "img/spots/breeze/attack/06_Bomb B Shock Dart/02.png",
+          "img/spots/breeze/attack/06_Bomb B Shock Dart/03.png",
+          "img/spots/breeze/attack/06_Bomb B Shock Dart/04.png",
+          "img/spots/breeze/attack/06_Bomb B Shock Dart/05.png",
+          "img/spots/breeze/attack/06_Bomb B Shock Dart/06.png"
+        ]
+      }
     ],
-  },
-};
-
-export default MapsSpots;
+    "defense": [
+      {
+        "name": "B Lobby",
+        "thumbnail": "img/spots/breeze/defense/01_B Lobby/logo.png",
+        "images": [
+          "img/spots/breeze/defense/01_B Lobby/01.png",
+          "img/spots/breeze/defense/01_B Lobby/02.png",
+          "img/spots/breeze/defense/01_B Lobby/03.png",
+          "img/spots/breeze/defense/01_B Lobby/04.png"
+        ]
+      },
+      {
+        "name": "B Lobby",
+        "thumbnail": "img/spots/breeze/defense/02_B Lobby/logo.png",
+        "images": [
+          "img/spots/breeze/defense/02_B Lobby/01.png",
+          "img/spots/breeze/defense/02_B Lobby/02.png",
+          "img/spots/breeze/defense/02_B Lobby/03.png",
+          "img/spots/breeze/defense/02_B Lobby/04.png"
+        ]
+      },
+      {
+        "name": "A Cave",
+        "thumbnail": "img/spots/breeze/defense/03_A Cave/logo.png",
+        "images": [
+          "img/spots/breeze/defense/03_A Cave/01.png",
+          "img/spots/breeze/defense/03_A Cave/02.png",
+          "img/spots/breeze/defense/03_A Cave/03.png",
+          "img/spots/breeze/defense/03_A Cave/04.png"
+        ]
+      },
+      {
+        "name": "Bomb B Shock Dart",
+        "thumbnail": "img/spots/breeze/defense/04_Bomb B Shock Dart/logo.png",
+        "images": [
+          "img/spots/breeze/defense/04_Bomb B Shock Dart/01.png",
+          "img/spots/breeze/defense/04_Bomb B Shock Dart/02.png",
+          "img/spots/breeze/defense/04_Bomb B Shock Dart/03.png",
+          "img/spots/breeze/defense/04_Bomb B Shock Dart/04.png",
+          "img/spots/breeze/defense/04_Bomb B Shock Dart/05.png"
+        ]
+      },
+      {
+        "name": "Bomb A Shock Dart",
+        "thumbnail": "img/spots/breeze/defense/05_Bomb A Shock Dart/logo.png",
+        "images": [
+          "img/spots/breeze/defense/05_Bomb A Shock Dart/01.png",
+          "img/spots/breeze/defense/05_Bomb A Shock Dart/02.png",
+          "img/spots/breeze/defense/05_Bomb A Shock Dart/03.png",
+          "img/spots/breeze/defense/05_Bomb A Shock Dart/04.png",
+          "img/spots/breeze/defense/05_Bomb A Shock Dart/05.png"
+        ]
+      },
+      {
+        "name": "Bomb A Shock Dart",
+        "thumbnail": "img/spots/breeze/defense/06_Bomb A Shock Dart/logo.png",
+        "images": [
+          "img/spots/breeze/defense/06_Bomb A Shock Dart/01.png",
+          "img/spots/breeze/defense/06_Bomb A Shock Dart/02.png",
+          "img/spots/breeze/defense/06_Bomb A Shock Dart/03.png",
+          "img/spots/breeze/defense/06_Bomb A Shock Dart/04.png",
+          "img/spots/breeze/defense/06_Bomb A Shock Dart/05.png"
+        ]
+      }
+    ]
+  }
+}; export default MapsSpots;
